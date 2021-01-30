9edfee8663eb38217c684ad2ed3a1a5e5f1fdd07
delrotf <delrotf02@gmail.com>
2021-01-30 14:28:27 -0500

    1 file modified

--- pages/TypingTrainer.tsx
+++ pages/TypingTrainer.tsx
@@ -1,20 +1,12 @@
-
 import React, { useContext, useEffect, useState } from "react";
-import { TypingContext } from "../context/typing-context";
-import { useEventListener } from "../hooks/useEventListener";
-import styled from 'styled-components'
 import { TypeBox } from "../components/TypeBox";
 
 const TypingTrainer = props => {
-  useEffect(() => {
-      console.log('Rendered')
-  })
-
   return (
-    <div className='typing-trainer p-5'>
-      <TypeBox/>
+    <div className="typing-trainer p-5">
+      <TypeBox />
     </div>
-  )
-}
+  );
+};
 
-export { TypingTrainer }
+export { TypingTrainer };
