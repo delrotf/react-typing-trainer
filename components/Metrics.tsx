59c3ea1f4d3a8e768912f5f519935163b2de0e7a
delrotf <delrotf02@gmail.com>
2021-01-30 14:47:11 -0500

    1 file modified

--- components/Metrics.tsx
+++ components/Metrics.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import React, { useContext } from "react";
+import { TypingContext } from "../context/typing-context";
 
 const Metrics = props => {
+  const { text, typedTexts} = useContext(TypingContext)
+
   return <div>Metrics</div>;
 };
 
