9440cc51145cab9b87de9444501267a14be83931
delrotf <delrotf02@gmail.com>
2021-02-01 13:00:35 -0500

    4 files modified

--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -3,7 +3,7 @@ import { Jumbotron } from "react-bootstrap";
 import { LoginContext } from "../context";
 
 const HomePage = props => {
-  const { firstname, authenticated } = useContext(LoginContext);
+  const { username, authenticated } = useContext(LoginContext);
 
   useEffect(() => {
     if (!authenticated) {
@@ -14,7 +14,7 @@ const HomePage = props => {
   return (
     <div>
       <Jumbotron>
-        <h1>Hello, {firstname}!</h1>
+        <h1>Hello, {username}!</h1>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
         </p>
--- pages/TypingTrainer/TypingTrainer.tsx
+++ pages/TypingTrainer/TypingTrainer.tsx
@@ -2,22 +2,36 @@ import React, { useContext, useEffect, useReducer, useState } from "react";
 import { Metrics } from "../../components/Metrics/Metrics";
 import { Timer } from "../../components/Timer/Timer";
 import { TypeBox } from "../../components/TypeBox/TypeBox";
+import { LoginContext } from "../../context";
 import { TypingContext } from "../../context/typing-context";
-import './TypingTrainer.scss'
+import "./TypingTrainer.scss";
 
 const TypingTrainer = props => {
+  const { username, authenticated } = useContext(LoginContext);
+
+  useEffect(() => {
+    if (!authenticated) {
+      props.history.push("/");
+    }
+  }, [authenticated]);
+
   const { timerSecCount, secondsLapsed } = useContext(TypingContext);
 
   const time = new Date();
   time.setSeconds(time.getSeconds() + timerSecCount);
 
   return (
-    <div className="typing-trainer p-5">
+    <div className="typing-trainer">
       <div>
-        <Timer expiryTimestamp={time} />
-        <TypeBox />
-        <div className={!secondsLapsed ? 'hidden' : 'ontop'}>
-          <Metrics />
+        <div className="accent py-3">Hello, {username}</div>
+        <div className="d-flex justify-content-center p-5">
+          <div>
+            <Timer expiryTimestamp={time} />
+            <TypeBox />
+            <div className={!secondsLapsed ? "hidden" : "ontop"}>
+              <Metrics />
+            </div>
+          </div>
         </div>
       </div>
     </div>
