6e96463dc9a915e1ad7aafbbda3b6097246fba8d
delrotf <delrotf02@gmail.com>
2021-02-01 14:39:20 -0500

    2 files modified

--- components/FormCard/FormCard.tsx
+++ components/FormCard/FormCard.tsx
@@ -5,7 +5,7 @@ import "./FormCard.scss";
 const FormCard = props => {
   return (
     <div className="form-card border">
-      <div className="header text-center p-2 text-light">You're my type!</div>
+      <div className="header text-center p-2">Typing Trainer</div>
       <div>
         <LoginForm {...props} />
       </div>
